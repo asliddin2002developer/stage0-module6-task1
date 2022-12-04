26988fe2d9f02b2ae7aae91bc5bf5ea3425a162b
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-05 01:44:17 +0500

    OOP

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,21 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    String getDescription(){
+        String fur = hasFur ? "a":"no";
+        String paws = numberOfPaws > 1 ? "paws":"paw";
+        String s;
+        s = "This animal is mostly " + this.color+ ". " + "It has " + this.numberOfPaws +" "+paws +" and " + fur + " fur.";
+        return s;
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,16 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
 
+    Bird() {
+        super("blue", 2, false);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Bird().getDescription());
+    }
+    @Override
+    String getDescription(){
+        return super.getDescription() + " Moreover, it has 2 wings and can fly.";
+    }
 }
