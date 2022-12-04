926a29700a79b54c8dc55759f15530f0c6249e85
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-05 01:47:30 +0500

    OOP updated

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -6,9 +6,6 @@ public class Bird extends Animal{
         super("blue", 2, false);
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Bird().getDescription());
-    }
     @Override
     String getDescription(){
         return super.getDescription() + " Moreover, it has 2 wings and can fly.";
